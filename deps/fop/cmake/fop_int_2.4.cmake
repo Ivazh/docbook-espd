d06a205af3eead4315c856c6bc3f88febf66a8ca
Anatoliy Kikot <a.v.kikot@yandex.ru>
2020-03-18 23:40:26 +0300

    [deps] Обновлен FOP до версии 2.4 (применяется по-умолчанию).
    Версия 2.3 доступна с применением свойства FOP_VERSION=2.3 при подключении проекта.

---
+++ deps/fop/cmake/fop_int_2.4.cmake
@@ -0,0 +1,21 @@
+#****************************************************************************************
+#*** Скрипт подключения встроенного в проект форматера Fop версии 2.3.                ***
+#***==================================================================================***
+#*** В скрипте создаётся псевдо-цель:                                                 ***
+#***   JAVA::fop - app IMPORTED GLOBAL.                                               ***
+#***                                                                                  ***
+#*** Редакция: 1                                                                      ***
+#*** Авторы: А.В. Кикоть (a.v.kikot@yandex.ru)                                        ***
+#*** Институт телекоммуникаций (c) 2019                                               ***
+#****************************************************************************************
+
+# Создание псевдо-цели.
+set(FOP_EXECUTABLE "${PROJECT_SOURCE_DIR}/fop-2.4/build/fop.jar")
+set(FOP_LIBDIR "${PROJECT_SOURCE_DIR}/fop-2.4/lib")
+set(FOP_VERSION "2.4")
+#
+add_executable(JAVA::fop IMPORTED GLOBAL)
+set_property(TARGET JAVA::fop PROPERTY IMPORTED_LOCATION "${FOP_EXECUTABLE}")
+set_property(TARGET JAVA::fop PROPERTY FOP_VERSION "${FOP_VERSION}")
+message(STATUS "JAVA::fop | path: ${FOP_EXECUTABLE}")
+message(STATUS "JAVA::fop | version: ${FOP_VERSION}")
